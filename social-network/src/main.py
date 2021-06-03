6df3e30a2f39dd2cb2fb4007b080d33f364d5e80
Luiz Agner <luizagnern@hotmail.com>
2021-06-03 19:42:55 -0300

    Implementing printing wall from console

--- social-network/src/main.py
+++ social-network/src/main.py
@@ -1,5 +1,8 @@
 from src.posts import posts_repository
 from src.posts.post import Post
+from src.wall.wall_service import WallService
+
+printer = print
 
 
 def run():
@@ -12,10 +15,19 @@ def perform_action(user_action: str):
     if ' -> ' in user_action:
         post_message(*user_action.split(' -> '))
         return
+    if ' wall' in user_action:
+        print_wall(user_action.strip(' wall'))
+        return
 
 
 def post_message(user, message):
     posts_repository.resolve().save_post(Post(user, message))
+
+
+def print_wall(user):
+    posts = WallService(user).build_wall()
+    for post in posts:
+        printer('{} - {}'.format(post.user, post.text))
 
 
 if __name__ == '__main__':
--- social-network/test/test_main.py
+++ social-network/test/test_main.py
@@ -5,6 +5,9 @@ from src.posts import posts_repository
 
 class TestMain(unittest.TestCase):
 
+    def setUp(self):
+        main.printer = mocked_print
+
     def test_post_message(self):
         main.perform_action('Rafaelfo -> e ai meus consagrados')
         main.perform_action('Morales -> jesus me salva')
@@ -15,6 +18,24 @@ class TestMain(unittest.TestCase):
 
         self.assertEqual('e ai meus consagrados', posts_1[0].text)
         self.assertEqual('jesus me salva', posts_2[0].text)
+
+    def test_print_wall(self):
+        main.perform_action('Rafaelfo -> e ai meus consagrados')
+        main.perform_action('Morales -> jesus')
+        main.perform_action('Morales -> me ajuda')
+        main.perform_action('Rafaelfo wall')
+        main.perform_action('Morales wall')
+
+        self.assertEqual('Rafaelfo - e ai meus consagrados', printed[0])
+        self.assertEqual('Morales - jesus', printed[1])
+        self.assertEqual('Morales - me ajuda', printed[2])
+
+
+printed = []
+
+
+def mocked_print(text):
+    printed.append(text)
 
 
 if __name__ == '__main__':
